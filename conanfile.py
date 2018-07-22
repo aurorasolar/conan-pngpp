1fbc3fed1ab4b4b608c010882056d3558511a2b5
Artalus <artalus-mail@yandex.ru>
2018-07-22 14:06:52 +0300

    licensing

--- conanfile.py
+++ conanfile.py
@@ -8,12 +8,13 @@ class PngConan(ConanFile):
     version = "0.2.9"
     settings = "compiler", "os"
     requires = 'libpng/1.6.34@bincrafters/stable'
-    license = "<Put the package license here>"
+    homepage = "http://www.nongnu.org/pngpp/"
     url = "https://github.com/Artalus/conan-pngpp"
+    license = "http://www.nongnu.org/pngpp/license.html"
+    exports = ["COPYING"]
     description = "png++: a C++ wrapper library for libpng"
     no_copy_source = True
     # No settings/options are necessary, this is header only
-    foldername = 'png++-%s' % version
 
     def patch_strerror(self, d):
         if self.settings.compiler != "Visual Studio" and self.settings.os != "Macos":
@@ -32,9 +33,10 @@ class PngConan(ConanFile):
 
     def source(self):
         dl = 'http://download.savannah.nongnu.org/releases/pngpp'
-        filename = '%s/%s.tar.gz' % (dl, self.foldername,)
+        foldername = 'png++-%s' % self.version
+        filename = '%s/%s.tar.gz' % (dl, foldername)
         tools.get(filename)
-        os.rename(self.foldername, "png++")
+        os.rename(foldername, "png++")
 
         self.patch_strerror("png++")
         # self.run("git clone ...") or
@@ -43,6 +45,7 @@ class PngConan(ConanFile):
 
     def package(self):
         self.copy("png++/*.hpp", "include")
+        self.copy("copying", "licenses", src="png++")
 
     def package_id(self):
         self.info.header_only()
