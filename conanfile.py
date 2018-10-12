15b99c599943ac880fb6b033f649c801092826fa
Artalus <artalus-mail@yandex.ru>
2018-10-12 14:22:02 +0300

    don't patch anything

--- conanfile.py
+++ conanfile.py
@@ -16,21 +16,6 @@ class PngConan(ConanFile):
     no_copy_source = True
     # No settings/options are necessary, this is header only
 
-    def patch_strerror(self, d):
-        if self.settings.compiler != "Visual Studio" and self.settings.os != "Macos":
-            return
-
-        with open("%s/error.hpp"%d, 'r') as f:
-            lines = f.readlines()
-        N = 108-1
-        print(lines[N])
-        assert(lines[N] == '''            return std::string(strerror_r(errnum, buf, ERRBUF_SIZE));\n''')
-
-        lines[N] = '''            return std::string(std::strerror(errnum));\n'''
-        with open("%s/error.hpp"%d, 'w') as f:
-            lines = f.writelines(lines)
-
-
     def source(self):
         dl = 'http://download.savannah.nongnu.org/releases/pngpp'
         foldername = 'png++-%s' % self.version
@@ -38,7 +23,6 @@ class PngConan(ConanFile):
         tools.get(filename)
         os.rename(foldername, "png++")
 
-        self.patch_strerror("png++")
         # self.run("git clone ...") or
         # tools.download("url", "file.zip")
         # tools.unzip("file.zip" )
